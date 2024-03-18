da35b013763cec9038076c8ed945ba0d8a57cbf7
justin-a-nobody <155355958+justin-a-nobody@users.noreply.github.com>
2024-03-18 13:53:27 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,8 +48,8 @@
 </script>
 
 <svelte:head>
-  <title>Solar API demo</title>
-  <meta name="description" content="Solar API demo" />
+  <title>Helios Solar Maps</title>
+  <meta name="description" content="Solar Maps" />
 </svelte:head>
 
 <main class="surface on-surface-text body-medium flex flex-col w-screen h-screen">
